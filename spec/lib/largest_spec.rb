578f9d3b0d49ef05a8382f06d494060cedd449bb
Punpnnn <optimus_pun@hotmail.com>
2014-10-14 15:37:48 +0700

    ef

--- spec/lib/largest_spec.rb
+++ spec/lib/largest_spec.rb
@@ -4,8 +4,8 @@ require "largest"   # name of the class we have just created
 describe Largest do
   describe "#find_largest" do
     it "finds the largest number in the array" do
-      largest_number = Largest.find_largest([1,4,2])
-      expect(largest_number).to eq(4)
+      largest_number = Largest.find_largest([1,5,2])
+      expect(largest_number).to eq(5)
     end
     it "finds the largest number in the array" do
       largest_number = Largest.find_largest([1,3,-3])
